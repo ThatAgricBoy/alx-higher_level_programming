79c571f04a82931fa07fd2d52d010dfc05aefa4d
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-15 10:06:29 -0700

    sorted

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,7 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
-        print(key, ':', a_dictionary[key])
+        print("{}: {}".format(key, a_dictionary[key]))
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
