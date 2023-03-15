77dd6243ff32fec97c52604f02fd23ccbac010d3
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-15 09:16:39 -0700

    diff

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,8 +2,10 @@
 
 def only_diff_elements(set_1, set_2):
     diff = set()
+    i = 0
+    j = 0
     for element in set_1:
         for element in b:
-            if a[i] != b[i]
+            if a[i] != b[j]
                 diff.add(element)
         return diff
