0724348fcbbe280088b4a152423202f42a6a99e9
ThatAgricBoy <samueljohn3999@gmail.com>
2023-04-04 13:42:36 -0700

    19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-def copy_list(l):
-    
-    """ 
-    Use slicing to create a shallow copy of the input list
-    The [:] notation specifies a slice that includes all 
-    elements of the list from the beginning to the end
-    This effectively creates a new list with the same elements as the original list
-    """
-    return l.copy()
+def copy_list(a):
+    return a[:]
