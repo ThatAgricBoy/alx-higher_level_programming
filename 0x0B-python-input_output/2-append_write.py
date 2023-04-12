adbea92f9f23424d4d916c7af2be276a6fec76d9
ThatAgricBoy <samueljohn3999@gmail.com>
2023-04-12 14:50:41 -0700

    appending

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,6 +12,6 @@ def append_write(filename="", text=""):
         appending text to a file
     """
     with open(filename, 'a', encoding="utf-8") as f:
-        append_data = f.write(text)
+        appended_data = f.write(text)
 
-    return len(append_data)
+    return appended_data
