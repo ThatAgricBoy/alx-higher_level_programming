1942f5e676b98cbaa3b41e361f04236495251c84
ThatAgricBoy <samueljohn3999@gmail.com>
2023-04-11 18:39:42 -0700

    0-look

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,4 +5,23 @@
     of available attributes and methods of an object
 """
 def lookup(obj):
-    return print(dir(obj)
+    # Get the object's class
+    cls = obj.__class__
+    
+    # Get the object's instance dictionary
+    instance_dict = obj.__dict__
+    
+    # Get the class dictionary
+    class_dict = cls.__dict__
+    
+    # Combine the instance and class dictionaries
+    attr_dict = {**class_dict, **instance_dict}
+    
+    # Filter out private and special attributes
+    attrs = [attr for attr in attr_dict.keys() if not attr.startswith("__")]
+    
+    # Sort the attributes alphabetically
+    attrs.sort()
+    
+    # Return the list of attributes
+    return attrs
