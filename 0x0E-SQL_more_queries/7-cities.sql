dd317bf2c9d3a0b02ed002c3fa4049ef4dec8cc5
ThatAgricBoy <samueljohn3999@gmail.com>
2023-05-17 07:02:08 -0700

    7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+-- a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	`id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	FOREIGN KEY(states_id) REFERENCES states(id)
+);
