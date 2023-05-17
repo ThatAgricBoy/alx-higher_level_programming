4e2c5a315e2477ca9699422f30153d8a0f4e6afb
ThatAgricBoy <samueljohn3999@gmail.com>
2023-05-17 06:48:50 -0700

    5.

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 -- a script that creates the table unique_id
 CREATE TABLE IF NOT EXISTS unique_id (
-	`id` INT DEFAULT UNIQUE 1,
+	`id` INT DEFAULT 1 UNIQUE,
 	`name` VARCHAR(256)
 );
