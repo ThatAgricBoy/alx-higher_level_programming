0c267043b71b9f8faa6654d0b6eaa40065619bf5
Thatagricguy <samueljohn3999@gmail.com>
2023-06-13 19:36:41 +0100

    102

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const addMeMaybe = (number, theFunction) => {
-  const addMeMaybe = number + 1;
-  theFunction(incrementedNumber);
+  const addMeMaybe = number ++;
+  theFunction(addMeMaybe);
 };
 
 exports.addMeMaybe = addMeMaybe;
