d1a3c9fe0d4b121ff4657a0b66868188fd82b1b3
Thatagricguy <samueljohn3999@gmail.com>
2023-06-16 17:15:35 +0100

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-from MySQLdb import _mysql
--- a script that lists all states from the database hbtn_0e_0_usa
-db=_mysql.connect(port=3306, user="joebob",
-                  password="moonpie",database="hbtn_0e_0_usa")
-db.query("""SELECT * FROM states ORDER BY id ASC""")
+""" a script that lists all states
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name == '__main__':
+
+    db_connect = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                 password=argv[2], database=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
