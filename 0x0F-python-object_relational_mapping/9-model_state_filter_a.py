d221eedb908f6c5f13b50a3736c40cfa863d4d99
Thatagricguy <samueljohn3999@gmail.com>
2023-06-17 11:19:48 +0100

    9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    """Get MySQL username, password, and database
+    name from command line arguments"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db_name = sys.argv[3]
+
+    """Create engine and session"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(mysql_username, mysql_password,
+                                  mysql_db_name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Query all State objects that contain the letter "a" and sort by id"""
+    states = session.query(State).filter(State.name.like('%a%'))
+    .order_by(State.id).all()
+
+    """Print results"""
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
