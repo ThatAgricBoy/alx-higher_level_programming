10282e14c3d6092fd257e7105991c91a384c0d31
Thatagricguy <samueljohn3999@gmail.com>
2023-06-17 10:50:19 +0100

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == '__main__':
-    """Get MySQL username, password, and database name from command line arguments
-"""
+    """Get MySQL username, password, and database name from command line arguments"""
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     mysql_db_name = sys.argv[3]
@@ -25,4 +24,3 @@ if __name__ == '__main__':
     """Print results"""
     for state in states:
         print('{}: {}'.format(state.id, state.name))
-        
