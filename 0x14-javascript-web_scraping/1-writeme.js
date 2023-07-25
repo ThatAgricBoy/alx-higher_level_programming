7a06c6302f3c3a1f964bb9ce402a8f2bff2828fb
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-25 16:38:46 +0100

    write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
-// script that writes a string to a file.
+// script to write to a file
 
 const fs = require('fs');
 
+// Get the file path and the string to write from command-line arguments
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
+// Write the content to the file using utf-8 encoding
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
+    // If an error occurred during writing, print the error object
     console.error(err);
   } else {
     console.log(`Content written to ${filePath} successfully.`);
