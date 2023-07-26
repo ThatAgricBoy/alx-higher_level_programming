a421c1081b9c039da591667ebdad6fc1c10d7683
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-26 16:36:25 +0100

    corrections

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,21 +11,20 @@ request.get(url, { json: true }, (error, response, body) => {
 
   const tasksCompleted = {};
 
-  // Loop through each user ID (key) in the response
-  for (const userId in body) {
-    // Get the array of to-do items for the current user
-    const todos = body[userId];
+  // Loop through each to-do item in the response array
+  body.forEach((todo) => {
+    const userId = todo.userId;
 
     // Initialize the tasksCompleted count for the current user
-    tasksCompleted[userId] = 0;
+    if (!tasksCompleted[userId]) {
+      tasksCompleted[userId] = 0;
+    }
 
-    // Loop through each to-do item and count completed tasks for the user
-    todos.forEach((todo) => {
-      if (todo.completed) {
-        tasksCompleted[userId]++;
-      }
-    });
-  }
+    // Increment the completed task count for the current user
+    if (todo.completed) {
+      tasksCompleted[userId]++;
+    }
+  });
 
   console.log(tasksCompleted);
 });
