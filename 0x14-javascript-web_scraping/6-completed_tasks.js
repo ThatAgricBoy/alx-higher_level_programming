54aa2574532655774e8572fee01d592961aae1da
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-26 16:50:19 +0100

    corrections

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,35 +5,19 @@ const url = process.argv[2];
 
 request.get(url, { json: true }, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
     return;
   }
 
   const tasksCompleted = {};
-
-  if (!Array.isArray(body)) {
-    console.error('Invalid response format. Expected an array.');
-    return;
-  }
-
   body.forEach((todo) => {
-    // Ensure that each todo item has the required properties (userId and completed)
-    if (typeof todo.userId === 'number' && typeof todo.completed === 'boolean') {
-      const userId = todo.userId.toString(); // Convert to string for consistency
-
-      // Initialize the tasksCompleted count for the current user
-      if (!tasksCompleted[userId]) {
-        tasksCompleted[userId] = 0;
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
       }
-
-      // Increment the completed task count for the current user
-      if (todo.completed) {
-        tasksCompleted[userId]++;
-      }
-    } else {
-      console.error('Invalid todo item format. Skipping:', todo);
     }
   });
-
   console.log(tasksCompleted);
 });
