9fcb74d61c1b1229086fb51a67c2430fd09ffb2e
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-26 17:30:49 +0100

    corrections

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,22 +5,31 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
+request.get(url, { json: true }, (error, response, data) => {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
+  const charactersUrls = data.characters;
+
+  // Function to fetch character data and print their names
+  function fetchCharacterName(characterUrl) {
+    request.get(characterUrl, { json: true }, (error, response, characterData) => {
       if (error) {
-        console.log(error);
-        return;
+        console.error(error);
+      } else {
+        console.log(characterData.name);
       }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+
+      // Fetch the next character's name until all characters are printed
+      const nextCharacterUrl = charactersUrls.shift();
+      if (nextCharacterUrl) {
+        fetchCharacterName(nextCharacterUrl);
+      }
     });
   }
+
+  // Start fetching character names
+  fetchCharacterName(charactersUrls.shift());
 });
