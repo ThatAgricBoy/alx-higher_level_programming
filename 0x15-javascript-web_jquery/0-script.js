e6c8deaabacd10c91c5e050805e4119213f72bfd
ThatAgricBoy <samueljohn3999@gmail.com>
2023-08-02 19:22:08 +0100

    no jquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,6 +1,4 @@
-#/usr/bin/node
-// updates the text color of the <header> element to red (#FF0000)
-document.addEventListener('DOMContentLoaded'), function () {
-    const header = document.querySelector('header');
-    header.style.color = '#FF000';
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('header');
+  header.style.color = '#FF0000';
 });
